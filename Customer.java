08161e029d7f9c3a30a86c7f4a9ef4cb28770825
Felipe Aragão <felipeanf@ufmg.br>
2021-03-11 10:58:51 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -16,6 +16,31 @@ public class Customer {
    public String getName (){
       return _name;
    }
+
+
+   private double amountFor(Rental each){
+    double thisAmount = 0;
+
+    //determine amounts for each line
+    switch (each.getMovie().getPriceCode()) {
+       case Movie.REGULAR:
+          thisAmount += 2;
+          if (each.getDaysRented() > 2)
+             thisAmount += (each.getDaysRented() - 2) * 1.5;
+          break;
+       case Movie.NEW_RELEASE:
+          thisAmount += each.getDaysRented() * 3;
+          break;
+       case Movie.CHILDRENS:
+          thisAmount += 1.5;
+          if (each.getDaysRented() > 3)
+             thisAmount += (each.getDaysRented() - 3) * 1.5;
+           break;
+    }
+
+    return thisAmount;
+
+   }
   
   public String statement() {
      double totalAmount = 0;
@@ -23,25 +48,12 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
+        double thisAmount;
         Rental each = (Rental) rentals.nextElement();
+        
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        thisAmount = amountFor(each);
+
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -52,6 +64,7 @@ public class Customer {
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
             String.valueOf(thisAmount) + "\n";
+     
         totalAmount += thisAmount;
 
      }
