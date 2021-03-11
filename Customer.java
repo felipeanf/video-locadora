28bf23a1d0d6d9fac38fb021db65a62236fcceee
Felipe Aragão <felipeanf@ufmg.br>
2021-03-11 11:23:33 -0300

    Commit 6

--- Customer.java
+++ Customer.java
@@ -25,13 +25,10 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
             
-
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
-
+        // add renter points
+        frequentRenterPoints += each.getFrequentRenterPoints();
+            
+        
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
             String.valueOf(each.getCharge()) + "\n";
